1aceef2c0d1cf50cf7085946a8356cef6f0d5b74
JCS <joshsad2@gmail.com>
2017-09-14 06:37:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nowServing(person) {
   if (person.length === 0) {
     return 'There is nobody waiting to be served!';
   } else {
-    current = person[0];
+    var current = person[0];
     person.splice(0, 1);
     return 'Currently serving ' + current + '!';
   }
