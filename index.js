4e77e349add07f0b538676f75b357ebb2be63c8e
JCS <joshsad2@gmail.com>
2017-09-14 06:36:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
   return 'Welcome, ' + name  + '. You are number ' + katzDeliLine.length + ' in line.';
 }
+
+function nowServing(person) {
+  if (person.length === 0) {
+    return 'There is nobody waiting to be served';
+  } else {
+    current = person[0];
+    person.splice(0, 1);
+    return 'Currently serving ' + current + '.';
+  }
+}
